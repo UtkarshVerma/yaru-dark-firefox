53a140a3f8d5a3959c9232243412844cdcc740d2
Utkarsh Verma <utkarsh.naveen@gmail.com>
2019-12-06 07:22:05 +0530

    Initial commit

--- zip.py
+++ zip.py
@@ -1,23 +1,19 @@
 from zipfile import ZipFile
 import os
 from shutil import rmtree
-from glob import glob
+from pathlib import Path
+rmtree("Builds", ignore_errors=True)
+os.makedirs("Builds")
+folder = "Yaru Dark Theme"
 
-rmtree('Builds', ignore_errors=True)
-os.makedirs('Builds')
+with ZipFile(f"Builds/firefox.zip", "w") as zf:
+	for file in Path(folder).rglob("*"):
+		zf.write(f"{file}", os.path.basename(file))
 
-
-build_folders = ['Matte Black (Cyan) Theme Firefox', 'Matte Black (Pink) Theme Firefox', 'Matte Black (Blue) Theme Firefox', 'Matte Black (Grey) Theme Firefox', 'Matte Black (Orange) Theme Firefox', 'Matte Black (Red) Theme Firefox', 'Matte Black (Violet) Theme Firefox', 'Matte Black (White) Theme Firefox', 'Dark Knight Joker Theme Firefox']
-for folder in build_folders:
-    
-    with ZipFile(f'Builds/{folder}.zip', 'w') as zf:
-        for file in glob(f'{folder}/*'):
-            zf.write(f'{file}', os.path.basename(file))
-
-
-folder = 'Matte Black Theme Chrome'
-with ZipFile(f'Builds/{folder}.zip', 'w') as zf:
-        for file in glob(f'{folder}/*'):
-            zf.write(f'{file}', os.path.basename(file))
-        for icon in os.listdir(f'{folder}/icons'):  # add icons to archive
-            zf.write(f'{folder}/icons/{icon}', f'icons/{icon}')
+'''
+with ZipFile("Builds/chrome.zip", "w") as zf:
+	for file in glob("{folder}/*"):
+		zf.write("{file}", os.path.basename(file))
+	for icon in os.listdir("{folder}/icons"):  # add icons to archive
+		zf.write("{folder}/icons/{icon}", "icons/{icon}")
+'''
